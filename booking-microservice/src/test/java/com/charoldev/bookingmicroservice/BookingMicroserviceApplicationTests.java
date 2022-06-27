0b27a97e9a9a1e94ffd5c2282009ed3c91ccf18b
charold-legion-s7 <harolrop14@gmail.com>
2022-06-26 22:10:03 -0500

    booking-microservice init with java 11

--- booking-microservice/src/test/java/com/charoldev/bookingmicroservice/BookingMicroserviceApplicationTests.java
+++ booking-microservice/src/test/java/com/charoldev/bookingmicroservice/BookingMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bookingmicroservice;
+package com.charoldev.bookingmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
