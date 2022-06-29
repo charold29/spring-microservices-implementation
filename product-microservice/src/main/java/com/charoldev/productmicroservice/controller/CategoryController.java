09ee15f353784899bf44bd20baa04948dc341d11
Harold Rojas <harolrop14@gmail.com>
2022-06-29 14:23:49 -0500

    refresh config vault hashicorp only for booking-ms

---
+++ product-microservice/src/main/java/com/charoldev/productmicroservice/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.charoldev.productmicroservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/categories")
+@Getter
+@Setter
+@RefreshScope
+public class CategoryController {
+
+    @Value("${app.testProperty}")
+    private String testProperty;
+
+    @GetMapping("/test-property")
+    public String getTestProperty(){
+        return this.testProperty;
+    }
+}
